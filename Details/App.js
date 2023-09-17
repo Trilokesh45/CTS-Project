c5fb1c06638a8429b4540395843df9231e2982f2
Trilokesh Rajasekaran S <138507357+Trilokesh45@users.noreply.github.com>
2023-09-17 20:25:39 +0530

    Create App.js

---
+++ Details/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    age: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Name: ${formData.name}\nEmail: ${formData.email}\nAge: ${formData.age}`);
+  };
+
+  return (
+    <div className="App">
+      <h1>User Details</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        /><br /><br />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        /><br /><br />
+
+        <label htmlFor="age">Age:</label>
+        <input
+          type="number"
+          id="age"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+          required
+        /><br /><br />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
