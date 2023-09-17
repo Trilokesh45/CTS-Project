a6ac66ebc26fa2b7f041762f11b0fbb0e0c85825
Trilokesh Rajasekaran S <138507357+Trilokesh45@users.noreply.github.com>
2023-09-17 20:31:35 +0530

    Create App.js

---
+++ Symptomatic Analysis/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SymptomForm from './components/SymptomForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Symptom Tracker</h1>
+      <SymptomForm />
+    </div>
+  );
+}
+
+
+export default App;
